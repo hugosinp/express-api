0c8d7705da999c2e7a7dca7898f58dd4d53c4da8
hugosinp <hugo.anaponn@gmail.com>
2022-05-02 21:02:18 +0200

    added isAdmin field on user Model

--- src/routes/api/users/userModel.js
+++ src/routes/api/users/userModel.js
@@ -4,6 +4,7 @@ import uniqueValidator from 'mongoose-unique-validator'
 const userModel = mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: false }
 });
 
 userModel.plugin(uniqueValidator);
