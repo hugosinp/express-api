1ac850160961ae1be59ee42ad4aba4dfa345af3f
hugosinp <hugo.anaponn@gmail.com>
2022-05-15 21:08:54 +0200

    updated middleware

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -5,20 +5,16 @@ const admin = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
         const userId = decodedToken._id;
 
-        if(req.body._id && req.body._id !== userId) {
-            throw 'Invalid user ID';
-        } 
-        
-        const user = await User.findOne({ _id: req.body._id });
+        const user = await User.findOne({ _id: userId });
 
         if(user.isAdmin === true) {
             next();
         } else {
-            throw 'Unauthorized';
+            throw 'Unauthorized.';
         }
+        
 
     } catch (error) {
         res.status(401).json({
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,19 +2,24 @@ import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
     try {
+        
+        // Token validity check
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         const userId = decodedToken._id;
 
-        if(req.body._id && req.body._id !== userId) {
+        // User _id validity check for POST & PUT request 
+        if(req.body.userId && req.body.userId !== userId) {
             throw 'Invalid user ID';
         } else {
             next();
         }
+        
     } catch (error) {
+        console.error("❌ Unvalid Token : " + error.message);
         res.status(403).json({
-            error: error,
+            error: "Unvalid Token",
             statusCode: 403
         })
     }
